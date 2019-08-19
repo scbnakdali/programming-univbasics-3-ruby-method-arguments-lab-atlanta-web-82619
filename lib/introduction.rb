eecb451de9550ccfe2ee457af2c8eca97b656bf4
Sarah Bullard Nakdali <scadmusbullard@gmail.com>
2019-08-19 00:56:52 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,15 +5,15 @@
 
 
 def introduction(name)
-  "Hi my name is #{name}."
+  "Hi, my name is #{name}."
 end 
 
 def introduction_with_language(name, language)
-  "Hi my name is #{name} and I am learning to program in #{language}."
+  "Hi, my name is #{name} and I am learning to program in #{language}."
 end 
 
 def introduction_with_language_optional(name, language = "Ruby")
-  "Hi my name is #{name} and I am learning to program in #{langauge}."
+  "Hi, my name is #{name} and I am learning to program in #{langauge}."
 end 
 
   
